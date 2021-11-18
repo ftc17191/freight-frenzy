3839f241bfc5840c7c5cd8f927e235f251e4f497
averagedolphin <t8wilhelm@gmail.com>
2021-11-18 16:22:54 -0600

    Going to work on FTC Layers revhub to work with Config

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/Config.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/Config.java
@@ -1,5 +1,11 @@
 package org.firstinspires.ftc.teamcode.config;
 
+import org.ftc17191.ftclayer.hardware.revhub.RevHub;
+
 public class Config
 {
+    static RevHub control_hub;
+    static RevHub expansion_hub;
+
+    
 }
